160ed51251c8a79c289c8b93f9cdeee003f6f0cf
Krishnan Mani <km@krishnanm.com>
2015-09-20 21:59:54 +0530

    Changed scripts to allow more general use

--- src/main/automation/copy_website_content.sh
+++ src/main/automation/copy_website_content.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-# Copy content recursively from public_html to the destination website hosting bucket
+# Copy content recursively from source location to the destination website hosting bucket
 
 export SCRIPT_NAME="$0"
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
     echo "Missing arguments"
-    echo "Usage: $SCRIPT_NAME profile bucket-name"
+    echo "Usage: $SCRIPT_NAME profile source-location bucket-name"
     exit 1
 fi
 
-export BUCKET_NAME="$2"
-aws --profile $1 s3 sync ../public_html s3://${BUCKET_NAME}
+export SOURCE_LOCATION="$2"
+export BUCKET_NAME="$3"
+aws --profile $1 s3 sync ${SOURCE_LOCATION} s3://${BUCKET_NAME}
--- src/main/automation/s3_create_hosting_bucket.sh
+++ src/main/automation/s3_create_hosting_bucket.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# For the list of names
 # Create the s3 buckets for website hosting
 # Associate the website hosting policy and bucket public access policy
 
@@ -8,18 +7,13 @@ export SCRIPT_NAME="$0"
 if [ $# -ne 2 ]
 then
     echo "Missing arguments"
-    echo "Usage: $SCRIPT_NAME profile bucket-suffix"
-    echo "Usage: The script also expects a file 'users' in the same folder"
+    echo "Usage: $SCRIPT_NAME profile bucket-name"
     exit 1
 fi
 
-export BUCKET_SUFFIX="$2"
+export BUCKET_NAME="$2"
 export REGION_NAME="eu-west-1"
 
-while read username
-do
-    echo "For user ${username}"
-    export BUCKET_NAME="${username}.${BUCKET_SUFFIX}"
     aws --profile $1 s3 rb --force s3://${BUCKET_NAME}
     aws --profile $1 s3 mb s3://${BUCKET_NAME}
     aws --profile $1 s3 website s3://${BUCKET_NAME} --index-document index.html
@@ -28,4 +22,3 @@ do
     aws --profile $1 s3api put-bucket-policy --bucket ${BUCKET_NAME} --policy file://website_hosting_bucket_policy.json
     aws --profile $1 s3 cp index.html s3://${BUCKET_NAME}/
     echo "URL: http://${BUCKET_NAME}.s3-website-${REGION_NAME}.amazonaws.com"
-done < users
