930cf0fc8e418a9e41f0c606f711f1df824717be
Krishnan Mani <km@krishnanm.com>
2015-09-20 22:51:50 +0530

    Updated script to create CNAME for website hosted in s3 bucket

--- src/main/automation/create_blog_dns_names.sh
+++ src/main/automation/create_blog_dns_names.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
-# Create CNAME records for blogs for the usernames
+# Create CNAME record
 
 export SCRIPT_NAME="$0"
-if [ $# -ne 4 ]
+if [ $# -ne 6 ]
 then
     echo "Missing arguments"
-    echo "Usage: $SCRIPT_NAME profile sub-domain hosted-zone-id path-to-change-set"
+    echo "Usage: $SCRIPT_NAME profile bucket-name host-name sub-domain hosted-zone-id path-to-change-set"
     exit 1
 fi
 
-export SUB_DOMAIN="$2"
-export HOSTED_ZONE_ID="$3"
-export PATH_TO_CHANGE_SET="$4"
+export BUCKET_NAME="$2"
+export HOSTNAME="$3"
+export SUB_DOMAIN="$4"
+export HOSTED_ZONE_ID="$5"
+export PATH_TO_CHANGE_SET="$6"
 export REGION_NAME="eu-west-1"
 
-while read username
-do
-    echo "For user ${username}"
-    export BUCKET_NAME="${username}.${SUB_DOMAIN}"
-    export FQDN="${BUCKET_NAME}.equalexperts.in"
-    export POINTS_TO="${FQDN}.s3-website-${REGION_NAME}.amazonaws.com"
+    export FQDN="${HOSTNAME}.${SUB_DOMAIN}.equalexperts.in"
+    export POINTS_TO="${BUCKET_NAME}.s3-website-${REGION_NAME}.amazonaws.com"
     cp cname_resource_record_set.json.template cname_resource_record_set.json
     sed -i '' "s/FQDN/${FQDN}/" cname_resource_record_set.json
     sed -i '' "s/POINTS_TO/${POINTS_TO}/" cname_resource_record_set.json
 
     aws --profile $1 route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://${PATH_TO_CHANGE_SET}/cname_resource_record_set.json
-done < users
